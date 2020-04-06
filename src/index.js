0d59afe86ae45a35f643da8d4fe451a088bc653d
sun-shadow <sun-shadow@gmail.com>
2020-04-06 14:50:00 +0800

    虚拟dom

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-console.log('hello world')
+console.log('hello world');
 const React = {
-  createElement(...args) {
-    console.log(args)
+  createElement(tag, attrs, ...children) {
+    return {
+      tag,
+      attrs, 
+      children
+    }
   }
+};
+
+
+
+let str = 'jirengu';
+let styleObj = {
+  color: 'red',
+  fontSize: '30px'
 }
 
-function dom(...args) {
-  console.log('dom', args)
-}
+let vdom = (
+  <div className="wrap">
+    Hello {str} 
+    <button className="btn" onClick={()=> console.log('click me')}> Click me!</button>
+    <p style={styleObj}>I have style</p>
+  </div>
+  );
+console.log(vdom);
 
-let div = <div>hello </div>;
-
-let name = '饥人谷';
-let element = <h1>hello {{name}}</h1>
+// babel 测试
+//https://babeljs.io/repl#?browsers=&build=&builtIns=false&spec=false&loose=false&code_lz=DYUwLgBAbgJg9gWwgXggCgFAQgHhgSyggGNgBDAZwoDkyERkAiAdwCcyAHRgPi2wgASIYMDgQA3hTCsAvhD7YcAIwCuYMHAB2JclVr0mSsJsYQtAYWD5iAa2Ti0ASmTcSWinFAA6UQHM0AOSk1jYQ9AGOMq6WIWEgAIQ4APSq6lq8_LgcEFIAnqD2eaAA8koAVlEAkhAAFmRQIDlg-SDJHBm4SQRQHY4A3BjE7p4gPnD-sIj9QA&debug=false&forceAllTransforms=false&shippedProposals=false&circleciRepo=&evaluate=false&fileSize=false&timeTravel=false&sourceType=module&lineWrap=false&presets=&prettier=false&targets=&version=7.9.0&externalPlugins=%40babel%2Fplugin-transform-react-jsx%407.9.4
